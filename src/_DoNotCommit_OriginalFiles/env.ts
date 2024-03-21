b96e218d562ddf4c31bf05f2a93eba1cdaa728bf
arikawa_kanichi <43694066+kan-ichi@users.noreply.github.com>
2024-03-21 13:37:52 +0900

    Install Firebase

---
+++ src/_DoNotCommit_OriginalFiles/env.ts
@@ -0,0 +1,10 @@
+// プロジェクトの設定 - 全般 - マイアプリ - SDK の設定と構成 の情報
+export const FIREBASE_API_KEY = '***************************************';
+export const FIREBASE_AUTH_DOMAIN = '********************.firebaseapp.com';
+export const FIREBASE_PROJECT_ID = '********************';
+export const FIREBASE_STORAGE_BUCKET = '********************.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '************';
+export const FIREBASE_APP_ID = '1:************:web:**********************';
+
+// Realtime Database - データ タブを選択して画面に表示される情報
+export const FIREBASE_REALTIME_DATABASE_URL = 'https://********************-default-rtdb.firebaseio.com';
