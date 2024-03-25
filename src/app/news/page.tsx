e61da1f90f7e3eae3a37eba535a2ca589b074906
arikawa_kanichi <43694066+kan-ichi@users.noreply.github.com>
2024-03-26 00:26:33 +0900

    ニュース管理機能の試作３（antdで実装）

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,36 +1,32 @@
 'use client';
 import { NewsRecord } from '@/common/types/News';
+import { FormatDateUtils } from '@/common/utils/FormatDateUtils';
 import { DalNews } from '@/features/DalNews';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  IconButton,
-  Pagination,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from '@mui/material';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { Button, Popconfirm, Table, message } from 'antd';
+import { useRouter } from 'next/navigation';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export default function NewsListPage() {
+  const router = useRouter();
   const [newsList, setNewsList] = useState<NewsRecord[]>([]);
-  const [page, setPage] = useState(1);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
-  const [newsToDelete, setNewsToDelete] = useState<NewsRecord | null>(null);
+  const tableContainerRef = useRef<HTMLDivElement>(null);
+  const [tableRowsHeight, setTableRowsHeight] = useState(0);
+
+  const handleResize = useCallback(() => {
+    const tableContainer = tableContainerRef.current;
+    if (tableContainer) {
+      const headerHeight = tableContainer.offsetTop;
+      const remainingHeight = window.innerHeight - headerHeight;
+      tableContainer.style.height = `${remainingHeight}px`;
+      setTableRowsHeight(window.innerHeight - headerHeight - 120);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [handleResize]);
 
   useEffect(() => {
     const fetchNews = async () => {
@@ -44,105 +40,80 @@ export default function NewsListPage() {
     fetchNews();
   }, []);
 
-  const handleDelete = async () => {
+  const handleDelete = async (id: string) => {
     try {
-      if (newsToDelete) {
-        await DalNews.deleteNews(newsToDelete.id);
-        setNewsList(newsList.filter((news) => news.id !== newsToDelete.id));
-        setDeleteDialogOpen(false);
-        setNewsToDelete(null);
-      }
+      await DalNews.deleteNews(id);
+      setNewsList(newsList.filter((news) => news.id !== id));
+      message.success('ニュースが正常に削除されました');
     } catch (error) {
       console.error('Failed to delete news:', error);
+      message.error('ニュースの削除に失敗しました');
     }
   };
 
-  const handleChangePage = (event: React.ChangeEvent<unknown>, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(1);
-  };
-
-  const totalPages = Math.ceil(newsList.length / rowsPerPage);
-  const paginatedNews = newsList.slice((page - 1) * rowsPerPage, page * rowsPerPage);
+  const columns = [
+    {
+      title: '日付',
+      key: 'date',
+      render: (text: string, record: NewsRecord) => (
+        <>
+          {FormatDateUtils.yyyyMMdd(record.createdAt ?? null)}
+          <br />
+          {console.log(record.updatedAt)}
+          {FormatDateUtils.yyyyMMdd(record.updatedAt ?? null)}
+        </>
+      ),
+    },
+    {
+      title: 'タイトル',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      width: '50%',
+      title: '説明',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      width: '25%',
+      title: '操作',
+      key: 'action',
+      render: (text: string, record: NewsRecord) => (
+        <>
+          <Button className="mr-2" type="primary" onClick={() => router.push(`/news/details?id=${record.id}`)}>
+            詳細表示・編集
+          </Button>
+          <Popconfirm
+            title="本当に削除しますか？"
+            onConfirm={() => handleDelete(record.id)}
+            okText="はい"
+            cancelText="いいえ"
+          >
+            <Button danger>削除</Button>
+          </Popconfirm>
+        </>
+      ),
+    },
+  ];
 
   return (
-    <div className="flex flex-col h-screen">
-      <div id="header" className="py-4">
-        <Typography variant="h4" component="h1">
-          ニュース一覧
-        </Typography>
-        <Link href="/news/add">
-          <Button variant="contained" color="primary">
-            新規ニュース登録
-          </Button>
-        </Link>
+    <div className="min-h-screen grid grid-rows-[auto_1fr]">
+      <div className="mb-1 flex justify-between items-center">
+        <h1 className="text-3xl font-bold ml-10">ニュース</h1>
+        <Button className="align-middle mr-10" type="primary" onClick={() => router.push('/news/add')}>
+          新規ニュース登録
+        </Button>
       </div>
-
-      <div id="main" className="flex-grow overflow-auto">
-        <TableContainer component={Paper}>
-          <Table stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell>タイトル</TableCell>
-                <TableCell>説明</TableCell>
-                <TableCell>操作</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {paginatedNews.map((news) => (
-                <TableRow key={news.id}>
-                  <TableCell>{news.title}</TableCell>
-                  <TableCell>{news.description}</TableCell>
-                  <TableCell>
-                    <Link href={`/news/details?id=${news.id}`}>
-                      <IconButton aria-label="編集">
-                        <EditIcon />
-                      </IconButton>
-                    </Link>
-                    <IconButton
-                      aria-label="削除"
-                      onClick={() => {
-                        setNewsToDelete(news);
-                        setDeleteDialogOpen(true);
-                      }}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+      <div className="overflow-y-auto" ref={tableContainerRef}>
+        <Table
+          scroll={{ y: tableRowsHeight }}
+          dataSource={newsList}
+          columns={columns}
+          rowKey="id"
+          pagination={{ pageSize: 10 }}
+        />
       </div>
-
-      <div id="footer" className="py-4">
-        <Pagination count={totalPages} page={page} onChange={handleChangePage} />
-      </div>
-
-      <Dialog
-        open={deleteDialogOpen}
-        onClose={() => setDeleteDialogOpen(false)}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">確認</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">本当に削除しますか？</DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setDeleteDialogOpen(false)} color="primary">
-            キャンセル
-          </Button>
-          <Button onClick={handleDelete} color="primary" autoFocus>
-            削除
-          </Button>
-        </DialogActions>
-      </Dialog>
     </div>
   );
 }
