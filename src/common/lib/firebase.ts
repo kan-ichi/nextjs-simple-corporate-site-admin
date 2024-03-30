4668f2059f7db1a59d63361490171a6df86fe692
arikawa_kanichi <43694066+kan-ichi@users.noreply.github.com>
2024-03-30 15:14:14 +0900

    firebase.ts に Realtime Database 使用時の記述を追加

--- src/common/lib/firebase.ts
+++ src/common/lib/firebase.ts
@@ -3,6 +3,7 @@ import {
   FIREBASE_API_KEY,
   FIREBASE_APP_ID,
   FIREBASE_AUTH_DOMAIN,
+  FIREBASE_REALTIME_DATABASE_URL, // Realtime Database 使用時のみ必要
   FIREBASE_MESSAGING_SENDER_ID,
   FIREBASE_PROJECT_ID,
   FIREBASE_STORAGE_BUCKET,
@@ -11,6 +12,7 @@ import {
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_REALTIME_DATABASE_URL, // Realtime Database 使用時のみ必要
   projectId: FIREBASE_PROJECT_ID,
   storageBucket: FIREBASE_STORAGE_BUCKET,
   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
